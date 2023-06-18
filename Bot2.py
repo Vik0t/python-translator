3cde4c4d82246ef3072c28c3fbeec44ae194b299
Vera870 <127411923+Vera870@users.noreply.github.com>
2023-06-18 16:29:41 +0700

    Add files via upload

--- Bot2.py
+++ Bot2.py
@@ -7,24 +7,23 @@ bot = telebot.TeleBot('6191694817:AAGW7Q40wPTqsufZqRr9UJm1OVYHtkVy23w')
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text == '/start':
-        main_menu = types.InlineKeyboardMarkup() # создание клавиатуры
-        main_menu.row_width = 2 # выбор размера(ширины)
-        item1 = types.InlineKeyboardButton(text="Английский", callback_data = "английский")
-        item2 = types.InlineKeyboardButton(text="Русский", callback_data = "русский")
-        main_menu.add(item1)
-        main_menu.add(item2)
-        bot.send_message(message.chat.id, 'Привет. Введите язык для перевода (на который переводится) русский или английский:', reply_markup = main_menu)
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Английский")
+        item2=types.KeyboardButton("Русский")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(message.chat.id, 'Привет. Введите язык для перевода (на который переводится) русский или английский:', reply_markup = markup)
+        bot.register_next_step_handler(message, start1)
     else:
         bot.send_message(message.from_user.id, 'Напиши: /start')
-@bot.callback_query_handler(func=lambda call: True)
-def start1(call):
-    if call.data == 'английский':
-        bot.send_message(call.message.chat.id, "Введите текст для перевода :  ")
-        bot.register_next_step_handler(call.message, per)
-    elif call.data == 'русский':
-        bot.send_message(call.message.from_user.id, "Введите текст для перевода:  ")
-        bot.register_next_step_handler(call.message, per1)
-def per(message):
+def start1(message):
+    if message.text == 'Английский':
+        bot.send_message(message.chat.id, "Введите текст для перевода :  ")
+        bot.register_next_step_handler(message, en)
+    elif message.text == 'Русский':
+        bot.send_message(message.chat.id, "Введите текст для перевода:  ")
+        bot.register_next_step_handler(message, ru)
+def en(message):
         z = translator.translate(message.text, src = "ru", dest = 'en')
         tr_word = z.text
         bot.send_message(message.from_user.id, tr_word )
@@ -35,7 +34,7 @@ def per(message):
         with open('result.mp3','rb') as a_f:
             audio = a_f.read()
         bot.send_audio(message.chat.id,audio)
-def per1(message):
+def ru(message):
         z = translator.translate(message.text, src = "en", dest = 'ru')
         tr_word = z.text
         bot.send_message(message.from_user.id, tr_word )
